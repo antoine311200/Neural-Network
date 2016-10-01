d2859cdf147c3ff87ff949222735055073c7d3cf
Antoine Debouchage <NattackDev@users.noreply.github.com>
2016-10-01 19:17:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const Neutral = require("./neutral.js");
 
-const test = new Neutral(["x", "y", "z"]);
+const neuron = new Neutral(["x", "y", "z"]);
 
-// test.config(["x", "y"], "q", "+");
-// test.config(["q", "z"], "f", "*");
-
- test.config(
+neuron.config(
 	 [["x", "y"], "q", "+"],
 	 [["q", "z"], "f", "*"]
-	);
+);
 
-test.run({
+neuron.run({
 	"x": -2,
 	"y": 5,
 	"z": -4
